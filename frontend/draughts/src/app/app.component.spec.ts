e88c81db593092808a2dba23143e4c681cf17fb7
laucv <laura.canelada.velasco@alumnos.upm.es>
2020-07-10 01:28:23 +0200

    Fixed bugs

--- frontend/draughts/src/app/app.component.spec.ts
+++ frontend/draughts/src/app/app.component.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import {UserService} from './services/user.service';
+import {HttpClientModule} from '@angular/common/http';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientModule
+      ],
+      providers: [
+        UserService
       ],
       declarations: [
         AppComponent
@@ -14,15 +20,4 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'draughts'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('draughts');
-  });
 });
